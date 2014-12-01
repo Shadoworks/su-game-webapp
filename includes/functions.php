79d1ff28da6083d65e3c7dcf54c232ed03418754
Christian Hinrichs <info@shadoworks.de>
2014-12-01 22:01:44 +0100

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -1,66 +1,25 @@
 <?php
+define('secure', true);
+require_once('config.php');
 
-function send_email($from, $to, $subject, $message){
-
-	// Helper function for sending email
-	
-	$headers  = 'MIME-Version: 1.0' . "\r\n";
-	$headers .= 'Content-type: text/plain; charset=utf-8' . "\r\n";
-	$headers .= 'From: '.$from . "\r\n";
-
-	return mail($to, $subject, $message, $headers);
-}
-
-function get_page_url(){
-
-	// Find out the URL of a PHP file
-
-	$url = 'http'.(empty($_SERVER['HTTPS'])?'':'s').'://'.$_SERVER['SERVER_NAME'];
-
-	if(isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI'] != ''){
-		$url.= $_SERVER['REQUEST_URI'];
+function genPW($length=8)
+{
+	$dummy = array_merge(range('0', '9'), range('a', 'z'), range('A', 'Z')); // array('#','&','@','$','_','%','?','+','-'));
+	mt_srand((double)microtime()*1000000); // shuffle array
+	for ($i = 1; $i <= (count($dummy)*2); $i++)
+	{
+		$swap		= mt_rand(0,count($dummy)-1);
+		$tmp		= $dummy[$swap];
+		$dummy[$swap]	= $dummy[0];
+		$dummy[0]	= $tmp;
 	}
-	else{
-		$url.= $_SERVER['PATH_INFO'];
-	}
-
-	return $url;
+	return substr(implode('',$dummy),0,$length); // get password
 }
 
-function rate_limit($ip, $limit_hour = 20, $limit_10_min = 10){
-	
-	// The number of login attempts for the last hour by this IP address
-
-	$count_hour = ORM::for_table('reg_login_attempt')
-					->where('ip', sprintf("%u", ip2long($ip)))
-					->where_raw("ts > SUBTIME(NOW(),'1:00')")
-					->count();
-
-	// The number of login attempts for the last 10 minutes by this IP address
-
-	$count_10_min =  ORM::for_table('reg_login_attempt')
-					->where('ip', sprintf("%u", ip2long($ip)))
-					->where_raw("ts > SUBTIME(NOW(),'0:10')")
-					->count();
-
-	if($count_hour > $limit_hour || $count_10_min > $limit_10_min){
-		throw new Exception('Too many login attempts!');
-	}
+function checkMail()
+{
+	if(preg_match('/^[^\x00-\x20()<>@,;:\\".[\]\x7f-\xff]+(?:\.[^\x00-\x20()<>@,;:\\".[\]\x7f-\xff]+)*\@[^\x00-\x20()<>@,;:\\".[\]\x7f-\xff]+(?:\.[^\x00-\x20()<>@,;:\\".[\]\x7f-\xff]+)+$/i', @$_POST["email"]))
+	return true;
+	return false;
 }
-
-function rate_limit_tick($ip, $email){
-
-	// Create a new record in the login attempt table
-
-	$login_attempt = ORM::for_table('reg_login_attempt')->create();
-
-	$login_attempt->email = $email;
-	$login_attempt->ip = sprintf("%u", ip2long($ip));
-
-	$login_attempt->save();
-}
-
-function redirect($url){
-	header("Location: $url");
-	exit;
-}
+?>
